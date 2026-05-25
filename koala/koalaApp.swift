daab87833360a6ba521e546da76a80b92cd046ff
Dimas Maulana Ahmad <97804178+dimsmaul@users.noreply.github.com>
2026-05-25 23:14:38 +0700

    Initial Commit

---
+++ koala/koalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  koalaApp.swift
+//  koala
+//
+//  Created by Dimas Maulana on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct koalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ koalaTests/koalaTests.swift
@@ -0,0 +1,17 @@
+//
+//  koalaTests.swift
+//  koalaTests
+//
+//  Created by Dimas Maulana on 25/05/26.
+//
+
+import Testing
+@testable import koala
+
+struct koalaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
